b06b0ca4ce08fda0fd890c885ebaf38e8142ccc5
tuvoksg1 <tuvoksg1@hotmail.com>
2018-10-16 17:17:39 +0100

    Empty IDP project

---
+++ Starter Files/ImageGallery/src/Marvin.IDP/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Test;
+
+namespace Marvin.IDP
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>();
+        }
+    }
+}
